5114b98429e9ee5a5ae8f4b16a064f9747074b65
dltmdgh0611 <dltmdgh0611@gmail.com>
2021-08-17 23:40:18 +0900

    ParsingData Class 추가, Singleton 적용

---
+++ Nasigo-Parser/ParsingData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nasigo_Parser
+{
+    public class ParsingData
+    {
+        public string ParsingData_Str { get; set; }
+        public List<string> ParsingData_List { get; set; }
+
+    }
+}
