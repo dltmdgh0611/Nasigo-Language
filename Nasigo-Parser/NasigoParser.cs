8b7e73899140e4f7221918be704de33e7c375206
dltmdgh0611 <dltmdgh0611@gmail.com>
2021-08-17 23:40:18 +0900

    ParsingData Class 추가, Singleton 적용

--- Nasigo-Parser/NasigoParser.cs
+++ Nasigo-Parser/NasigoParser.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.IO;
 
 namespace Nasigo_Parser
 {
-    public class NasigoParser
+    public class NasigoParser : NPSingleton<NasigoParser>
     {
+        public ParsingData DoParse(string path)
+        {
+            ParsingData result = new ParsingData();
+
+            try
+            {
+                if (!ParsingLine(ref result)) throw new Exception("Parsing Error... \n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+
+            return result;
+        }
+        
+        private bool ParsingLine(ref ParsingData data)
+        {
+            
+            return true;
+        }
     }
 }
