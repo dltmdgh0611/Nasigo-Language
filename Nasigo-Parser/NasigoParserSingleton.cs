fef0a86c41aba47230e82f82d9babf5a8dce99b3
dltmdgh0611 <dltmdgh0611@gmail.com>
2021-08-17 23:40:18 +0900

    ParsingData Class 추가, Singleton 적용

---
+++ Nasigo-Parser/NasigoParserSingleton.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nasigo_Parser
+{
+    public class NPSingleton<T> where T : class, new()
+    {
+        protected NPSingleton() { }
+
+        private static readonly Lazy<T> instance = new Lazy<T>(() => new T());
+
+        public static T Instance { get { return instance.Value; } }
+    }
+}
--- NasigoLanguage/Nasigolang.cs
+++ NasigoLanguage/Nasigolang.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Nasigo_Parser;
 
 namespace NasigoLanguage
 {
@@ -6,7 +7,7 @@ namespace NasigoLanguage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NasigoParser.Instance.DoParse("");
         }
     }
 }
