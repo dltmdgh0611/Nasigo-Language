760ef94f066f485ef7f20c0790aedf9b547eef39
dltmdgh0611 <dltmdgh0611@gmail.com>
2021-08-22 14:51:15 +0900

    Token Class, Lexer Base 구성

---
+++ NasigoLanguage/Nasigo-Lexer/NasigoLexer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NasigoLanguage
+{
+    public class NasigoLexer : NPSingleton<NasigoLexer>
+    {
+        static int Cursor = -1;
+        static int line = 0;
+        List<string> list = null;
+       
+
+        char nextChar()
+        {
+            char ch = 'a';
+
+            if(Cursor == list[line].Length)
+            {
+                ch = list[line][Cursor];
+                line++;
+                Cursor = -1;
+            }
+
+            return ch;
+        }
+
+        void Automata_Var()
+        {
+            char ch = 'a';
+            ch = nextChar();
+            if (isletter(ch)) goto state2;
+
+            state2: ch = nextChar();
+            if (isletter(ch) || isdigit(ch)) goto state2;
+            else goto state3;
+
+            state3: backchar();
+        }
+
+
+
+        
+
+        void DoLexicalAnalysis(ParsingData parsingData)
+        {
+            list = parsingData.ParsingData_List;
+        }
+
+        
+        
+    }
+}
---
+++ NasigoLanguage/Nasigo-Lexer/Token.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NasigoLanguage
+{
+    public class Token
+    {
+        Key Kind { get; set; }
+
+        string _word = null;
+        string word { get => _word;
+            set
+            {
+                if (value.ToString().Length > 30) throw new Exception("error : 개체 이름의 최대 길이는 30입니다.");
+                else _word = value.ToString();
+            }
+        }
+        int value { get; set; }
+    }
+
+    public class Key
+    {
+
+    }
+}
