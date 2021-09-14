4f6f4d34cb31950a5fe71bd4a818191b2506316c
dltmdgh0611 <dltmdgh0611@gmail.com>
2021-09-15 00:10:41 +0900

    LLparser 업로드

---
+++ NasigoLanguage/Nasigo-LL-Parser/NasigoLLParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NasigoLanguage.Nasigo_LL_Parser
+{
+    class NasigoLLParser
+    {
+        void block(int blockIndex, Token token)
+        {
+             
+            while (true)
+            {
+                switch (token.kind)
+                {
+                    case Kind.Int:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
